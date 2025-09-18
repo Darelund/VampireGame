9d8c6f3607e27d284c1fbc3794c4fd03a6317300
Darelund <93180040+Darelund@users.noreply.github.com>
2025-09-18 17:56:05 +0200

    Removed double spawntext

--- Assets/Scripts/VisualManager.cs
+++ Assets/Scripts/VisualManager.cs
@@ -25,12 +25,7 @@ public class VisualManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
             SpawnText("Test", Vector2.zero, TextType.Normal);
     }
-    public void SpawnText(string text, Vector2 spawnPos, TextType textType)
-    {
-        var spawnedText = Instantiate(textPrefabs.Find(t => t.GetComponent<FeedbackText>().TextType == textType).gameObject, spawnPos, Quaternion.identity);
-        spawnedText.GetComponent<FeedbackText>().Initialize(text);
-    }
-    public void SpawnText(string text, Vector2 spawnPos, TextType textType, Color newColor)
+    public void SpawnText(string text, Vector2 spawnPos, TextType textType, Color newColor = new Color())
     {
         var spawnedText = Instantiate(textPrefabs.Find(t => t.GetComponent<FeedbackText>().TextType == textType).gameObject, spawnPos, Quaternion.identity);
         spawnedText.GetComponent<FeedbackText>().Initialize(text, newColor);
