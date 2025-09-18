60a4b66fad1752171740ef680c0661af55b3469b
Darelund <93180040+Darelund@users.noreply.github.com>
2025-09-18 16:17:42 +0200

    Enemy scriptableobject added
    I started working on a scriptableobject for enemies

---
+++ Assets/SO/BaseEnemySO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BaseEnemySO", menuName = "Scriptable Objects/BaseEnemySO")]
+public class BaseEnemySO : ScriptableObject
+{
+    public float Speed;
+    public float Damage;
+    public float Health;
+}
