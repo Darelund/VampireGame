0263ffe70d0eeedf42ca405de3efb70aa0ef7d9e
Darelund <93180040+Darelund@users.noreply.github.com>
2025-09-29 11:03:52 +0200

    Updated MainMenu
    Added more menus to options

--- Assets/Scripts/MainMenu/OptionState.cs
+++ Assets/Scripts/MainMenu/OptionState.cs
@@ -1,7 +1,36 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionState : State
 {
-    
+    [SerializeField] private List<GameObject> optionPanels = new List<GameObject>();
+    [SerializeField] private List<GameObject> optionBtns = new List<GameObject>();
+    [SerializeField] private Color activeColor;
+    [SerializeField] private Color disabledColor;
+
+
+
+    public void SetActivePanel(int index)
+    {
+        optionPanels.ForEach(p => { 
+            p.SetActive(false);
+        });
+        optionBtns.ForEach(b => {
+            b.GetComponent<Image>().color = disabledColor;
+        });
+
+        optionBtns[index].GetComponent<Image>().color = activeColor;
+        optionPanels[index].SetActive(true);
+    }
+
+    public void CloseOptionPanel()
+    {
+        optionPanels.ForEach(p => {
+            p.SetActive(false);
+        });
+        //The first panel that should be active when you enter settings
+        optionPanels[0].SetActive(true); 
+    }
    
 }
