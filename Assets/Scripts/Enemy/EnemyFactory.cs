b49049aa403cf7a1a168d1b11a584cfd15e9f08d
Darelund <93180040+Darelund@users.noreply.github.com>
2025-10-08 13:08:03 +0200

    Spawning now works
    -The error I had at home is now gone... somehow, didn't change the code(from what I know)
    -Instead of loading resources and using its data format (like .png, .prefab) i removed that .whatever. I am not sure if that matters.
    -Instead of using EnemyController as type when spawning enemies I used GameObject, it now works

--- Assets/Scripts/Enemy/EnemyFactory.cs
+++ Assets/Scripts/Enemy/EnemyFactory.cs
@@ -4,73 +4,42 @@ using UnityEngine;
 
 public class EnemyFactory : MonoBehaviour
 {
-   // private Builder _builder;
-    [SerializeField] private GameObject[] enemyPrefabs;
-    [SerializeField] private GameObject[] weaponsPrefabs;
+    //I am getting them through code instead, might change that later
+    //[SerializeField] private GameObject[] enemyPrefabs;
+    //[SerializeField] private GameObject[] weaponsPrefabs;
 
-    //private Dictionary<string, Factory<EnemyController>> factories = new();
     public readonly Dictionary<string, Func<EnemyController>> enemies = new();
 
 
     public EnemyFactory()
     {
-        //factories = new()
-        //{
-        //    ["RangeVätte"] = new Factory<EnemyController>(new EnemyStrategy(Resources.Load<EnemyController>("Enemy"), { new OrbitMovement, ))
-        //};
-        //enemies = new()
-        //{
-        //    ["RangedVätte"] = () => {
-        //        Factory<EnemyController> enemyFactory = new();
-        //        enemyFactory.builder.SetPrefab(Resources.Load<EnemyController>("Enemies/Vätte.prefab").gameObject);
-        //       return enemyFactory.builder.WithScriptAttribute<OrbitMovement>().WithPrefabAttribute(Resources.Load<EnemyController>("Weapons/RangedWeapon.prefab").gameObject).Build();
-        //    },
-        //    ["MeleeVätte"] = () => {
-        //        Factory<EnemyController> enemyFactory = new();
-        //        enemyFactory.builder.SetPrefab(Resources.Load<EnemyController>("Enemies/Vätte.prefab").gameObject);
-        //       return enemyFactory.builder.WithScriptAttribute<MoveTowardsMovement>().WithPrefabAttribute(Resources.Load<EnemyController>("Weapons/RangedWeapon.prefab").gameObject).Build();
-        //    },
-        //    ["RangedLyktgubbe"] = () => {
-        //        Factory<EnemyController> enemyFactory = new();
-        //        enemyFactory.builder.SetPrefab(Resources.Load<EnemyController>("Enemies/Lyktgubbe.prefab").gameObject);
-        //      return  enemyFactory.builder.WithScriptAttribute<OrbitMovement>().WithPrefabAttribute(Resources.Load<EnemyController>("Weapons/RangedWeapon.prefab").gameObject).Build();
-        //    },
-        //    ["RangedLyktgubbe"] = () => {
-        //        Factory<EnemyController> enemyFactory = new();
-        //        enemyFactory.builder.SetPrefab(Resources.Load<EnemyController>("Enemies/Lyktgubbe.prefab").gameObject);
-        //       return enemyFactory.builder.WithScriptAttribute<MoveTowardsMovement>().WithPrefabAttribute(Resources.Load<EnemyController>("Weapons/RangedWeapon.prefab").gameObject).Build();
-        //    },
-        //};
-    }
-    
-    //Han gjorde så att Abstract factory inte ärvar av Mono utan har det i en dictionary
-    //Istället för casen där nere borde du ha en klass där du gör flera enemies 
-    //som har olika typer
-    public EnemyController CreateEntity()
-    {
-        EnemyController enemy = null;
-        //switch (enemyType)
-        //{
-        //    case EnemyType.RangeVätte:
-        //        _builder.SetPrefab(enemyPrefabs[0]);
-        //        enemy = _builder.AddMovement<OrbitMovement>().AddWeapon(weaponsPrefabs[0]).Build();
-        //        break;
-        //    case EnemyType.MeleeVätte:
-        //        _builder.SetPrefab(enemyPrefabs[0]);
-        //        enemy = _builder.AddMovement<MoveTowardsMovement>().AddWeapon(weaponsPrefabs[1]).Build();
-        //        break;
-        //    case EnemyType.RangedLyktgubbe:
-        //        _builder.SetPrefab(enemyPrefabs[1]);
-        //        enemy = _builder.AddMovement<OrbitMovement>().AddWeapon(weaponsPrefabs[0]).Build();
-        //        break;
-        //    case EnemyType.MeleeLyktgubbe:
-        //        _builder.SetPrefab(enemyPrefabs[1]);
-        //        enemy = _builder.AddMovement<MoveTowardsMovement>().AddWeapon(weaponsPrefabs[1]).Build();
-        //        break;
-        //    default:
-        //        break;
-        //}
-        return enemy;
+        enemies = new()
+        {
+            ["RangedVätte"] = () =>
+            {
+                Factory<EnemyController> enemyFactory = new();
+                enemyFactory.builder.SetPrefab(Resources.Load<EnemyController>("Enemies/Vätte").gameObject);
+                return enemyFactory.builder.WithScriptAttribute<OrbitMovement>().WithPrefabAttribute(Resources.Load<GameObject>("Weapons/RangedWeapon")).Build();
+            },
+            ["MeleeVätte"] = () =>
+            {
+                Factory<EnemyController> enemyFactory = new();
+                enemyFactory.builder.SetPrefab(Resources.Load<EnemyController>("Enemies/Vätte").gameObject);
+                return enemyFactory.builder.WithScriptAttribute<MoveTowardsMovement>().WithPrefabAttribute(Resources.Load<GameObject>("Weapons/RangedWeapon").gameObject).Build();
+            },
+            ["RangedLyktgubbe"] = () =>
+            {
+                Factory<EnemyController> enemyFactory = new();
+                enemyFactory.builder.SetPrefab(Resources.Load<EnemyController>("Enemies/Lyktgubbe").gameObject);
+                return enemyFactory.builder.WithScriptAttribute<OrbitMovement>().WithPrefabAttribute(Resources.Load<GameObject>("Weapons/RangedWeapon").gameObject).Build();
+            },
+            ["RangedLyktgubbe"] = () =>
+            {
+                Factory<EnemyController> enemyFactory = new();
+                enemyFactory.builder.SetPrefab(Resources.Load<EnemyController>("Enemies/Lyktgubbe").gameObject);
+                return enemyFactory.builder.WithScriptAttribute<MoveTowardsMovement>().WithPrefabAttribute(Resources.Load<GameObject>("Weapons/RangedWeapon").gameObject).Build();
+            },
+        };
     }
 }
 //public class EnemyStrategy
@@ -79,21 +48,3 @@ public class EnemyFactory : MonoBehaviour
 //    public GameObject EnemyPrefab;
 //    public List<Attribute> ScriptAttributes = new();
 //    public List<GameObject> PrefabAttributes = new();
-
-
-//    public EnemyStrategy(GameObject enemyPrefab, List<Attribute> scriptAttributes, List<GameObject> prefabAttributes)
-//    {
-//        EnemyPrefab = enemyPrefab;
-//        ScriptAttributes = scriptAttributes;
-//        PrefabAttributes = prefabAttributes;
-//    }
-//    //public GameObject Weapon;
-
-//}
-//public enum EnemyType
-//{
-//    RangeVätte,
-//    MeleeVätte,
-//    RangedLyktgubbe,
-//    MeleeLyktgubbe
-//}
--- Assets/Scripts/Enemy/Factory.cs
+++ Assets/Scripts/Enemy/Factory.cs
@@ -3,13 +3,10 @@ using UnityEngine;
 public class Factory<T> where T : IAttributable, new()
 {
     public Builder<T> builder;
-   // private EnemyStrategy enemyStrategy;
 
     public Factory()
     {
         builder = new Builder<T>();
-        //this.enemyStrategy = enemyStrategy;
-        //builder.SetPrefab(enemyStrategy.EnemyPrefab);
     }
     public void CreateEntity()
     {
